0d1a1f523fda73ebf6966d9df264f59bb5fa6385
Annie Wiegel <annewiegel17@gmail.com>
2021-02-18 22:29:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-    elsif !@sender.valid? && @sender.balance < @amount
+    elsif !@sender.valid? && @sender.balance < @amount || sender.valid? == false
       return "Transaction rejected. Please check your account balance."
       @status = "rejected"
     end
