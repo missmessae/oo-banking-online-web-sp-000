350a11973bb5529c3df9d975dfcfc396b9a22a5b
Annie Wiegel <annewiegel17@gmail.com>
2021-02-18 22:16:47 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,7 +10,9 @@ class Transfer
 
   def valid?
     accounts = BankAccount.all.select {|acct| acct.name == @sender || acct.name == @receiver}
-    accounts.each {|a| a.valid?}
+    if accounts.each {|a| a.valid?}
+    else
+      return false
   end
 
   def execute_transaction
