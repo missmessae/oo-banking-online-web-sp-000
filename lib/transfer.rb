030a259d9b267703420342cc00d09168654dbde2
Annie Wiegel <annewiegel17@gmail.com>
2021-02-18 22:32:29 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,13 +17,18 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-    else 
+    else
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
   end
 
   def reverse_transfer
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    end
   end
 
 end
