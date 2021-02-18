92f3039ee2e4d3c5ab23f8551293059c46fa3bda
Annie Wiegel <annewiegel17@gmail.com>
2021-02-18 21:45:57 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -13,5 +13,31 @@ class BankAccount
 
   def display_balance
     puts "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    if @status == "open" && @balance > 0
+      return true
+    else
+      return false
+    end
+  end 
+  describe '#valid?' do
+    it "is valid with an open status and a balance greater than 0" do
+      @broke = BankAccount.new("Kat Dennings")
+      @broke.balance = 0
+      @closed = BankAccount.new("Beth Behrs")
+      @closed.status = "closed"
+      expect(avi.valid?).to eq(true)
+      expect(@broke.valid?).to eq(false)
+      expect(@closed.valid?).to eq(false)
+    end
+  end
+
+  describe '#close_account' do
+    it "can close its account" do
+      avi.close_account
+      expect(avi.status).to eq("closed")
+
 
 end
