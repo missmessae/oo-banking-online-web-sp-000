ad341a1845f9576c01aa75679da5a887a5cb4187
Annie Wiegel <annewiegel17@gmail.com>
2021-02-18 21:40:43 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,10 +2,12 @@ class BankAccount
   attr_accessor :name
 
   def initialize(name)
-    @name = name
+    @name = name       #need to figure out how to not allow name changes
     @balance = 1000
     @status = "open"
   end
+
+  def deposit(amount)
   
 
 end
