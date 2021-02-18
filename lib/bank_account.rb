3c94d8692c7c71b1b688c8a4d4cb0a2312a98cbc
Annie Wiegel <annewiegel17@gmail.com>
2021-02-18 21:46:38 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -21,18 +21,10 @@ class BankAccount
     else
       return false
     end
-  end 
-  describe '#valid?' do
-    it "is valid with an open status and a balance greater than 0" do
-      @broke = BankAccount.new("Kat Dennings")
-      @broke.balance = 0
-      @closed = BankAccount.new("Beth Behrs")
-      @closed.status = "closed"
-      expect(avi.valid?).to eq(true)
-      expect(@broke.valid?).to eq(false)
-      expect(@closed.valid?).to eq(false)
-    end
   end
+
+  def close_account
+    
 
   describe '#close_account' do
     it "can close its account" do
